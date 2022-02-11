72ac1c4dde6405a1c09310d0b965f6c6329a2cda
Riley Shimp <rilryshimp@gmail.com>
2022-02-11 08:40:59 -0800

    Add Euro class with static method to make API call

---
+++ src/js/euro.js
@@ -0,0 +1,17 @@
+export default class Euro {
+  static getEuro(input) {
+    return new Promise (function(resolve,reject) { 
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/EUR/AMOUNT`;
+      let request = new XMLHttpRequest();
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
