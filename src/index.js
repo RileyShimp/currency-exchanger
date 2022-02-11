060f03581c1ad70e8df5b2b6ddcabad529ddff33
Riley Shimp <rilryshimp@gmail.com>
2022-02-11 08:57:10 -0800

    WIP: add UI for Euro API call

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Euro from './js/euro.js';
 
+let clearInput = () => {
+  $('#search').val("");
+};
+
+$(document).ready(function() {
+  $("#euro").click(function() {
+    let input = $("#input").val();
+    clearInput();
+    let promise = Euro.getEuro(input);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      const conversion = body.conversion_result;
+      let htmlString = `<img src="${conversion}">`;
+      $(".conversionResult").html(htmlString);
+    },function(error) {
+      $('.showErrors').text(`There was an error: ${body.error-type}`);
+    });
+  });
+});
 
